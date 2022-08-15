1a4db2c93c88d34a187e2851a9bf18fc514bd47a
nick <92675266+sardormirzakomilov@users.noreply.github.com>
2022-08-15 12:53:32 +0500

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,22 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.max = max;
+    this.min = min;
+  }
 
-    }
+  guess() {
+    return Math.round((this.max + this.min) / 2);
+  }
 
-    guess() {
+  lower() {
+    this.max = this.guess();
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.guess();
+  }
 }
 
 module.exports = GuessingGame;
